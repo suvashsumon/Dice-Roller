6ef9f96eefc129594fb8a0866d4c26b0d7ebb6cf
Suvash Kumar <suvashkumar.naogaon@gmail.com>
2022-06-07 15:39:05 +0600

    dice image resourses are added and changing image file by clicking button implemented

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import java.util.*
@@ -20,8 +21,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val dicetext : TextView = findViewById(R.id.textview)
+        val dicetext : ImageView = findViewById(R.id.dice_image)
         val randInt = Random().nextInt(6) + 1
-        dicetext.text = randInt.toString()
+        val drawableResource = when (randInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        dicetext.setImageResource(drawableResource)
     }
 }
