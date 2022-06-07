68f23e6b6c8eaf11685c997230d19758a4c5b35e
Suvash Kumar <suvashkumar.naogaon@gmail.com>
2022-06-07 15:25:50 +0600

    design and findbyid implemented

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollBtn : Button = findViewById(R.id.rollbutton)
+        rollBtn.text = "Done"
     }
 }
