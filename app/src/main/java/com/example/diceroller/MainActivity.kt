07cc0e9f6422b24a135ec3b7a2a408175b27d236
Suvash Kumar <suvashkumar.naogaon@gmail.com>
2022-06-07 15:32:26 +0600

    dice text changing by clicking btn implemented

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,7 +14,14 @@ class MainActivity : AppCompatActivity() {
 
         val rollBtn : Button = findViewById(R.id.rollbutton)
         rollBtn.setOnClickListener {
-            Toast.makeText(this, "Hi, Aminul", Toast.LENGTH_SHORT).show()
+//            Toast.makeText(this, "Hi, Aminul", Toast.LENGTH_SHORT).show()
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        val dicetext : TextView = findViewById(R.id.textview)
+        val randInt = Random().nextInt(6) + 1
+        dicetext.text = randInt.toString()
     }
 }
