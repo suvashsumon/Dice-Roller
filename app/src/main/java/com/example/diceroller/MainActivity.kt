548594b91e0583adb0b64a5dcedb20bf29fcd9c1
Suvash Kumar <suvashkumar.naogaon@gmail.com>
2022-06-07 15:28:30 +0600

    setOnClickListener added to btn

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,6 +11,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollBtn : Button = findViewById(R.id.rollbutton)
-        rollBtn.text = "Done"
+        rollBtn.setOnClickListener {
+            Toast.makeText(this, "Hi, Aminul", Toast.LENGTH_SHORT).show()
+        }
     }
 }
